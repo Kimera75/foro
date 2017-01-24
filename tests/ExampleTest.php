ccb284c2b35b98e5ae506035c5cee6e3a96e825d
Kimera75 <etg75@hotmail.com>
2017-01-24 13:15:36 -0600

    Mensaje del commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+
+    //Se agregar esta linea para que cada que ejecutemos
+    //las pruebas, se eliminen los datos que ya habia en la  tabla
+    //y no nos marque error con los datos que hemos indicado que deben de ser unicos, como el email por ejemplo
+    //se puede utilizar el DatabaseMigrations, pero si son muchas las bd, entonces, cada vez que se haga una prueba sera lento, por lo cual se usa este otro elemento
+
+    //para que todo se haga dentro de una transaccion y queda vacia la tabla y no afecta nada 
+
+    use DatabaseTransactions;
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+
+       $user =  factory(\App\User::class)->create([
+            'name' => 'Edu Mors Mortis',
+            'email'=>'edu@edu.com'
+                ]);
+       
+
+       $this->actingAs($user,'api')
+
+             ->visit('api/user')
+             ->see('Edu Mors Mortis')
+             ->see('edu@edu.com');
+    }
+}
